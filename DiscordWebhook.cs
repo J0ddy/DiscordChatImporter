047fc7cfa835b1d8d6a6cf17e0e8960a24700dc8
J0ddy <me@joddy.dev>
2023-02-23 00:53:37 +0200

    Add project files.

---
+++ DiscordWebhook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordChatImporter
+{
+    public class DiscordWebhook : IDisposable
+    {
+        private readonly WebClient webClient;
+        private static NameValueCollection values = new NameValueCollection();
+        public string WebHook { get; set; }
+        public string UserName { get; set; }
+        public string ProfilePicture { get; set; }
+
+        public DiscordWebhook(string webHookUrl)
+        {
+            webClient = new WebClient();
+            WebHook = webHookUrl;
+            values.Add("username", "JOD.GG Message Importer");
+            values.Add("avatar_url", "https://i.jod.gg/YUXA1/feBAKOmO20/raw.png");
+        }
+
+
+        public void SendMessage(string msgSend)
+        {
+            values.Set("content", msgSend);
+            webClient.UploadValues(WebHook, values);
+        }
+
+        public void Dispose()
+        {
+            webClient.Dispose();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace DiscordChatImporter
+{
+    internal class Program
+    {
+        private static DiscordWebhook webhook;
+        private const string whurl = @"https://discord.com/api/webhooks/YOUR-WEBHOOK-HERE";
+        private static readonly string[] filter = { "**Embed:** null", "Embed: **null**" }; // Filter out certain messages
+
+        static void Main(string[] args)
+        {
+            List<string> files = new List<string>();
+            Console.WriteLine("Please enter the to the directory to import all files from: ");
+            string dir = Console.ReadLine();
+            if (!Directory.Exists(dir)) throw new DirectoryNotFoundException(nameof(dir));
+            files.AddRange(Directory.GetFiles(dir, "*.txt"));
+
+            List<string> messages = new List<string>();
+
+            foreach (string f in files)
+            {
+                string name = Path.GetFileName(f);
+                Console.Write("Reading " + name + "...");
+                messages.AddRange(ReadAndSplitFile(f));
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" Loaded!");
+                Console.ResetColor();
+            }
+
+            Console.Write("Initializing Discord webhook...");
+            webhook = new DiscordWebhook(whurl);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" Done!");
+            Console.ResetColor();
+            Console.WriteLine("Sending messages...\n");
+            SendMessages(messages);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\nDone!");
+            Console.ResetColor();
+            Console.WriteLine(" All Messages sent.");
+
+        }
+
+        private static void SendMessages(List<string> messages)
+        {
+            int counter = 0;
+            foreach (string message in messages)
+            {
+                Thread.Sleep(400);
+                Console.Write("Sending message " + ++counter + " of " + messages.Count + "...");
+                webhook.SendMessage(message);
+                webhook.Dispose();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" Sent!");
+                Console.ResetColor();
+            }
+        }
+
+        private static List<string> ReadAndSplitFile(string filePath)
+        {
+            List<string> lines = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            using StreamReader sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (filter.Any(line.Contains)) continue;
+                if (sb.Length + line.Length <= 1998)
+                {
+                    sb.Append(line+Environment.NewLine);
+                    if (sr.EndOfStream)
+                    {
+                        lines.Add(line);
+                        return lines;
+                    }
+                }
+                else
+                {
+                    lines.Add(sb.ToString());
+                    sb = new StringBuilder();
+                }
+            }
+
+            return lines;
+        }
+    }
+}
